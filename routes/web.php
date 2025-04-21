a07a50c59aac5d14544de84393c14a46148370ae
jeff.lee <jeff.lee@majorbio.com>
2025-04-21 23:54:34 +0800

    修改提交

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [FamilyController::class, 'index'])->name('index');
 
 // 家系
 Route::get('family/index', [FamilyController::class, 'index'])->name('family.index');
