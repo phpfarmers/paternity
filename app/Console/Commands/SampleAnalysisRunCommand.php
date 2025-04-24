7d82b56a883daf4d03f8bd78b6abf9dd49cee422
jeff.lee <jeff.lee@majorbio.com>
2025-04-24 08:28:42 +0800

    修改提交

--- app/Console/Commands/SampleAnalysisRunCommand.php
+++ app/Console/Commands/SampleAnalysisRunCommand.php
@@ -64,22 +64,22 @@ class SampleAnalysisRunCommand extends Command
                 if ($returnVar === 0) {
                     $this->info("找到以下文件:");
                     foreach ($output as $file) {
-                        $this->info($file);
+                        // $this->info($file);
                         // 获取文件名
                         $fileName = basename($file);
                         $this->info($fileName);
                     }
                     // 符合条件-更新检测结果状态为成功
-                    if(count($output) > 0){
+                    // if(count($output) > 0){
                         $sample->analysis_result = Sample::ANALYSIS_RESULT_SUCCESS;
                         $sample->analysis_time = date('Y-m-d');
                         $sample->save();
-                    }else{
-                        // 未下机-变为未检测-继续检测
-                        $this->info("文件数量不正确");
-                        $sample->analysis_result = Sample::ANALYSIS_RESULT_UNKNOWN;
-                        $sample->save();
-                    }
+                    // }else{
+                    //     // 未下机-变为未检测-继续检测
+                    //     $this->info("文件数量不正确");
+                    //     $sample->analysis_result = Sample::ANALYSIS_RESULT_UNKNOWN;
+                    //     $sample->save();
+                    // }
                 } else {
                     $this->error("未找到文件或命令执行失败");
                     // 不符合条件-更新检测结果状态为失败
