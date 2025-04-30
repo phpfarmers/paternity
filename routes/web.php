0e2897b6e2183d8c722b4d32eb25f73345b5c362
jeff.lee <jeff.lee@majorbio.com>
2025-04-30 15:01:51 +0800

    修改提交

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FamilyController;
 use App\Http\Controllers\SampleController;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -46,3 +47,21 @@ Route::get('sample/check-rerun', [SampleController::class, 'checkRerun'])->name(
 Route::get('sample/analysis-run', [SampleController::class, 'analysisRun'])->name('sample.analysisRun');
 // 样本分析重运行
 Route::get('sample/analysis-rerun', [SampleController::class, 'analysisRerun'])->name('sample.analysisRerun');
+
+Route::get('/test-redis', function () {
+    try {
+        Redis::set('test-key', 'test-value');
+        $value = Redis::get('test-key');
+        
+        return response()->json([
+            'status' => 'success',
+            'data' => $value,
+            'message' => 'Redis is working correctly'
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => phpinfo()
+        ], 500);
+    }
+});
