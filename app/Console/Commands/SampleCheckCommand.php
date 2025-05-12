f4ff051352e8a441e24eaf07cc3c0ad54c8dcd9a
jeff.lee <jeff.lee@majorbio.com>
2025-05-13 00:17:07 +0800

    修改提交

--- app/Console/Commands/SampleCheckCommand.php
+++ app/Console/Commands/SampleCheckCommand.php
@@ -58,7 +58,8 @@ class SampleCheckCommand extends Command
                 $searchPattern = escapeshellarg('*'.$sample->sample_name.'*.gz'); // 搜索模式-样本名
                 // $searchPattern = escapeshellarg('*Ignition.php'); // 测试
                 $searchPath = escapeshellarg($ossData); // 搜索路径
-                $command = "find {$searchPath} -name {$searchPattern} -type f -printf '%T@ %p\n' | sort -nr | cut -d' ' -f2-";
+                // $command = "find {$searchPath} -name {$searchPattern} -type f -printf '%T@ %p\n' | sort -nr | cut -d' ' -f2-";
+                $command = "find {$searchPath} -name {$searchPattern} -type f -exec ls -lt --time=ctime {} + | sort -k6,7 -k8 -r";
                 $this->info('执行命令：'.$command);
                 Log::info('执行命令：'.$command);
                 // 执行shell命令
